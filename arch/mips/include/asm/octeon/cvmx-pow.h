cdf24bf9f28a03167dfd8d2e1039e3a3a8b76d37
Aleksey Makarov <aleksey.makarov@auriga.com>
2015-03-11 15:02:51 +0300

    cvmx-pow.h: comment out some obscure stuff

--- arch/mips/include/asm/octeon/cvmx-pow.h
+++ arch/mips/include/asm/octeon/cvmx-pow.h
@@ -2688,6 +2688,7 @@ static inline void cvmx_pow_work_submit_node(cvmx_wqe_t * wqp, uint32_t tag,
 	cvmx_write_io(ptr.u64, tag_req.u64);
 }
 
+#if 0
 /**
  * This function sets the group mask for a core.  The group mask
  * indicates which groups each core will accept work from. There are
@@ -2710,6 +2711,7 @@ static inline void cvmx_pow_work_submit_node(cvmx_wqe_t * wqp, uint32_t tag,
  */
 static inline void cvmx_pow_set_group_mask(uint64_t core_num, uint64_t mask)
 {
+#if 0
 	if (octeon_has_feature(OCTEON_FEATURE_CN78XX_WQE)) {
 		const unsigned mask_set = 0;
 		cvmx_sso_ppx_sx_grpmskx_t grp_msk;
@@ -2747,7 +2749,9 @@ static inline void cvmx_pow_set_group_mask(uint64_t core_num, uint64_t mask)
 								    rix),
 					    grp_msk.u64);
 		}
-	} else if (octeon_has_feature(OCTEON_FEATURE_CN68XX_WQE)) {
+	} else
+#endif
+	if (octeon_has_feature(OCTEON_FEATURE_CN68XX_WQE)) {
 		cvmx_sso_ppx_grp_msk_t grp_msk;
 		grp_msk.s.grp_msk = mask;
 		cvmx_write_csr(CVMX_SSO_PPX_GRP_MSK(core_num), grp_msk.u64);
@@ -3025,6 +3029,7 @@ static inline void cvmx_pow_set_priority(uint64_t core_num,
 		cvmx_write_csr(CVMX_POW_PP_GRP_MSKX(core_num), grp_msk.u64);
 	}
 }
+#endif
 
 /**
  * Performs a tag switch and then an immediate deschedule. This completes
@@ -3326,6 +3331,7 @@ static inline uint64_t cvmx_sso3_get_wqe_count(int node)
 	return cnt;
 }
 
+#if 0
 static inline uint64_t cvmx_sso_get_total_wqe_count(void)
 {
 	if (octeon_has_feature(OCTEON_FEATURE_CN78XX_WQE)) {
@@ -3344,6 +3350,7 @@ static inline uint64_t cvmx_sso_get_total_wqe_count(void)
 		return (pow_iq_com_cnt.s.iq_cnt);
 	}
 }
+#endif
 
 /**
  * Store the current POW internal state into the supplied
